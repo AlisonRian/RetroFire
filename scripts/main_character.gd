9713b66d4b60b6b565163e2610216d3ae0ef5dba
Alison Rian <alisonrianbr@gmail.com>
2025-11-11 23:05:40 -0300

    refatorando

--- scripts/main_character.gd
+++ scripts/main_character.gd
@@ -5,12 +5,24 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const CAMERA_SENSE = 0.003
 
+var direction
+
 #const BOB_FREQ = 2.0
 #const BOB_AMP = 0.08
 #var t_bob = 0.0
+
 @onready var head: Node3D = $Head
 @onready var camera: Camera3D = $Head/Camera3D
-@onready var animation_player = get_node("male_casual/AnimationPlayer") as AnimationPlayer
+@onready var animation_tree: AnimationTree = $Player/AnimationTree
+
+enum States {
+	IDLE,
+	WALKING,
+	RUNNING,
+	SHOOTING,
+	JUMPING
+}
+var current_state = States.IDLE
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -25,25 +37,21 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
 	var input_dir := Input.get_vector("move_left","move_right","move_up","move_down")
-	var direction := (transform.basis * Vector3(-input_dir.x, 0, -input_dir.y)).normalized()
-	
-	#if Input.is_action_pressed("sprint"):
-		#speed = SPRINT_SPEED
-	#else: 
-		#speed = SPEED
-	
-	if direction:
-		animation_player.play("Animations/walking")
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		animation_player.play("Animations/idle")
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	direction = (transform.basis * Vector3(-input_dir.x, 0, -input_dir.y)).normalized()
+	#print(current_state)
+	match current_state:
+		States.IDLE:
+			process_idle(delta)
+		States.WALKING:
+			process_walking(delta)
+		States.RUNNING:
+			pass
+		States.JUMPING:
+			process_jumping()
+		States.SHOOTING:
+			pass
+
 	#
 	#t_bob += delta * velocity.length() * float(is_on_floor())
 	#camera.transform.origin = _headBob(t_bob)
@@ -55,3 +63,59 @@ func _physics_process(delta: float) -> void:
 	#pos.y = sin(time * BOB_FREQ) * BOB_AMP
 	#pos.x = cos(time * BOB_FREQ/2) * BOB_AMP
 	#return pos
+
+func change_state(new_state):
+	if current_state == new_state:
+		return
+	
+	#Fazer algo antes de trocar de estado, tipo tocar algum som.
+	#match current_state:
+		#States.JUMPING:
+		
+	current_state = new_state
+	match current_state:
+		States.IDLE:
+			animation_tree.set("parameters/Transition/transition_request", "idle")
+		States.WALKING:
+			animation_tree.set("parameters/Transition/transition_request","walk")
+		States.RUNNING:
+			animation_tree.set("parameters/Transition/transition_request", "run")
+		States.JUMPING:
+			animation_tree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+		States.SHOOTING:
+			pass
+
+func process_idle(delta):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+		change_state(States.JUMPING)
+		return
+	if direction:
+		change_state(States.WALKING)
+		return
+	velocity.x = lerp(velocity.x, 0.0, 5.0*delta)
+	velocity.z = lerp(velocity.z, 0.0, 5.0*delta)
+
+func process_jumping():
+	if is_on_floor():
+		change_state(States.IDLE)
+		return
+	
+
+func process_walking(delta):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		change_state(States.JUMPING)
+		return
+
+	if not direction:
+		change_state(States.IDLE)
+		return
+	
+	velocity.x = lerp(velocity.x, 0.0, 5.0*delta)
+	velocity.z = lerp(velocity.z, 0.0, 5.0*delta)
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
+		
+		
+		
